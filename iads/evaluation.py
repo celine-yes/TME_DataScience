44221bda956636fb78cd1427ec0e9a448c7847c3
celine-yes <celine21106@gmail.com>
2023-03-13 23:23:10 +0100

    debut tme6

--- iads/evaluation.py
+++ iads/evaluation.py
@@ -16,3 +16,50 @@ import pandas as pd
 # ------------------------ 
 #TODO: à compléter  plus tard
 # ------------------------ 
+
+def crossval_strat(X, Y, n_iterations, iteration):
+        # Séparation par classe
+    Xp = {}
+    Yp = {}
+    for i in range(len(X)):
+        if Y[i] not in Xp:
+            Xp[Y[i]] = []
+            Yp[Y[i]] = []
+        Xp[Y[i]].append(X[i])
+        Yp[Y[i]].append(Y[i])
+    
+    # Calculer le nombre d'exemples pour chaque ensemble de test
+    n_test = {}
+    for k, v in Xp.items():
+        n_test[k] = int(len(v) / n_iterations)
+    
+    # Extraire les exemples pour l'ensemble de test courant
+    Xtest = []
+    Ytest = []
+    for k, v in Xp.items():
+        start = iteration * n_test[k]
+        end = start + n_test[k]
+        Xtest += v[start:end]
+        Ytest += Yp[k][start:end]
+        
+    # Extraire les exemples pour l'ensemble d'apprentissage
+    Xapp = []
+    Yapp = []
+    for k, v in Xp.items():
+        Xapp += v[:iteration*n_test[k]] + v[(iteration+1)*n_test[k]:]
+        Yapp += Yp[k][:iteration*n_test[k]] + Yp[k][(iteration+1)*n_test[k]:]
+    
+    # Convertir en numpy arrays
+    Xapp = np.array(Xapp)
+    Yapp = np.array(Yapp)
+    Xtest = np.array(Xtest)
+    Ytest = np.array(Ytest)
+   
+    return Xapp, Yapp, Xtest, Ytest
+    
+
+def analyse_perfs(L):
+    """ L : liste de nombres réels non vide
+        rend le tuple (moyenne, écart-type)
+    """
+    return (sum(L)/len(L), np.std(L))  
